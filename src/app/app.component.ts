704d2f50a4a86a9b0871e47f69cb290ab8207c92
sunwei-hh <342451737@qq.com>
2020-05-28 09:47:03 +0800

    00

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'xj40';
+  title = 'APP启动了';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloWordComponent } from './hello-word/hello-word.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWordComponent,
+    UserItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
